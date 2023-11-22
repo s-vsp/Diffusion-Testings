7006261c3374b66c21f9cc9cc82c06185c988cb7
Kamil <kkwarciak@student.agh.edu.pl>
2023-11-22 14:48:15 +0100

    testing scheduler + Deforum animation

---
+++ __init__.py
@@ -0,0 +1,5 @@
+prompts = {
+    25: "On a moonless Halloween night, a dense fog enveloped the small town, casting an eerie silence over the deserted streets",
+    50: "As she explored the dimly lit corridors, whispers echoed, and shadows danced in the corners of her vision",
+    75: "Panic set in as the mirrors shattered, releasing the trapped spirits into the room"
+}
